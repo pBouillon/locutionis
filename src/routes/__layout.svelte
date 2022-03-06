5138e4ab52922be0e6815dcdd1c6cbc81119eb52
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-03-06 12:25:17 +0100

    Initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script>
+import "../app.css";
+</script>
+
+<div class="flex gap-5 p-5 items-baseline">
+  <p class="text-6xl">
+    Locutionis
+  </p>
+
+  <p>Petit référentiel des figures de style</p>
+</div>
+
+<slot />
