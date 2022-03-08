8dcf601ac66b6e0faccb1dea2dca2587f6ea60ee
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-03-08 19:23:30 +0100

    Create the mobile menu container

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,3 +1,7 @@
+<script>
+  let showMobileMenu = false;
+</script>
+
 <nav class="p-5 flex items-baseline justify-between">
 
   <div class="flex items-baseline gap-3">
@@ -10,6 +14,11 @@
       Petit référentiel des figures de style
     </p>
   </div>
+
+  <button type="button" class="md:hidden rounded flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-primary/25 focus:ring-offset-2 focus:ring-offset-slate-50"
+    on:click={() => (showMobileMenu = true)}>
+    <i class="bi bi-list text-2xl px-1"></i>
+  </button>
 
   <ul class="gap-5 hidden md:flex">
     <li>
@@ -31,14 +40,32 @@
     </li>
   </ul>
 
+  {#if showMobileMenu}
+    <div class="fixed z-50 inset-0 md:hidden">
+      <div class="overlay" on:click={() => (showMobileMenu = false)}>
+      </div>
+
+      <div class="mobile-menu-container">
+        xxx
+      </div>
+    </div>
+  {/if}
 </nav>
 
 <style>
+  .bg-gradient {
+    @apply text-transparent bg-clip-text bg-gradient-to-br from-indigo-600 to-primary;
+  }
+
+  .mobile-menu-container {
+    @apply fixed top-4 right-4 w-full max-w-xs bg-white rounded-lg shadow-lg p-6 text-base font-semibold text-slate-900;
+  }
+
   .navbar-link {
     @apply text-lg tracking-wide decoration-2 decoration-primary;
   }
 
-  .bg-gradient {
-    @apply text-transparent bg-clip-text bg-gradient-to-br from-secondary to-primary;
+  .overlay {
+    @apply fixed inset-0 bg-black/20 backdrop-blur-sm;
   }
 </style>
