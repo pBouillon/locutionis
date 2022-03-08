64e94170870a253f8a6055b45aca8b5604f2d0b9
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-03-08 20:18:28 +0100

    Generate the production bundle under docs/

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'docs',
+      assets: 'docs',
+      fallback: 'index.html'
+    }),
+    paths: {
+      base: dev ? '' : '/locutionis',
+    },
+  }
 };
 
 export default config;
