db5dafe4dd25f3dec04c5ca8daaa2dc701506675
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-03-09 20:03:20 +0100

    Fix the navigation to the home page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,3 +1,7 @@
+<script>
+  import { base } from '$app/paths';
+</script>
+
 <section class="flex flex-col gap-8 mt-8">
   <h1 class="text-4xl font-bold text-center text-primary">
     Page introuvable
@@ -8,6 +12,6 @@
   </p>
 
   <div class="text-lg text-center">
-    <a href="/" class="font-semibold link">Retourner à l'accueil</a>
+    <a href="{base}/" class="font-semibold link">Retourner à l'accueil</a>
   </div>
 </section>
