e6628f9f3f85debcd1f38c2cd35c7460d672149f
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-03-11 23:12:32 +0100

    Fix the creation of the glossary overriding the previous values of a key

--- src/routes/glossary/index.svelte
+++ src/routes/glossary/index.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-import { normalize } from "$lib/functions/utils";
+  import GlossaryEntry from "$lib/components/glossary-entry.svelte";
+  import { normalize } from "$lib/functions/utils";
 
   import type { FigureOfSpeech } from "$lib/models/figure-of-speech";
 
@@ -11,17 +12,16 @@ import { normalize } from "$lib/functions/utils";
       let key = definition.name[0].toLocaleLowerCase();
       key = normalize(key);
 
-      map[key] = definition;
+      map[key] = [...map.get(key) ?? [], definition];
       return map;
-    }, new Map<string, FigureOfSpeech[]>());
+    },
+    new Map<string, FigureOfSpeech[]>());
 </script>
 
 <h1 class="text-primary text-xl mb-5">
   Glossaire
 </h1>
 
-<pre>{JSON.stringify(glossary)}</pre>
-
-{#each Object.entries(glossary) as [ key, definition ]}
-  <p>[{key}] {definition.name}</p>
+{#each Object.entries(glossary) as [key, definitions]}
+  <GlossaryEntry {key} {definitions} />
 {/each}
