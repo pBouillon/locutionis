1151710eb03af3f21dd38b07419e3b82214641cc
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-03-11 22:01:47 +0100

    Bind the glossary to the list of the figures of speech

---
+++ src/routes/glossary/index.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type { FigureOfSpeech } from "$lib/models/figure-of-speech";
+
+
+  export let definitions: FigureOfSpeech[];
+</script>
+
+{#each definitions as definition}
+  <p>{definition.name}</p>
+{/each}
