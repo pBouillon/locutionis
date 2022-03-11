439376191527711f9358f5009ffd8b677c191978
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-03-11 19:41:25 +0100

    Load a figure of speech from the route

---
+++ src/routes/glossary/[name].svelte
@@ -0,0 +1,22 @@
+<script context='module' lang='ts'>
+  import type { FigureOfSpeech } from "$lib/models/figure-of-speech";
+
+  export const load = async ({ params, fetch }) => {
+    const name: string = params.name;
+
+    const response = await fetch('/api/figures-of-speech.json');
+    var definitions: FigureOfSpeech[] = await response.json();
+
+    const definition = definitions.filter(figureOfSpeech => figureOfSpeech.name === name)[0];
+
+    return {
+      props: { definition: definition },
+    };
+  }
+</script>
+
+<script lang='ts'>
+  export let definition: FigureOfSpeech;
+</script>
+
+<pre>{definition.name}</pre>
