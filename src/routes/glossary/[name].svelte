56b79133d94a085350df1d3efb59b189a21824b0
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-03-11 22:01:20 +0100

    Reformat the endpoint for a figure of speech

--- src/routes/glossary/[name].svelte
+++ src/routes/glossary/[name].svelte
@@ -1,6 +1,7 @@
 <script lang='ts'>
   import type { FigureOfSpeech } from "$lib/models/figure-of-speech";
 
+
   export let definition: FigureOfSpeech;
 </script>
 
--- src/routes/glossary/[name].ts
+++ src/routes/glossary/[name].ts
@@ -1,19 +1,16 @@
 import type { FigureOfSpeech } from "$lib/models/figure-of-speech";
 
+/** @type {import('./[name]').RequestHandler} */
 export const get = async ({ params }) => {
   const figuresOfSpeechFiles = import.meta.glob('../../lib/data/*.json');
 
-  if (!params?.name) {
-    return { status: 400 };
-  }
-
-  const definitions = (await Promise.all(
+  const [ definition ] = (await Promise.all(
     Object
       .values(figuresOfSpeechFiles)
       .map(async resolver => await resolver() as FigureOfSpeech)))
-      .filter(definition => definition.name === params?.name);
+    .filter(definition => definition.name === params?.name);
 
-  return definitions.length == 1
-    ? { body: { definition: definitions[0] }}
+  return definition
+    ? { body: { definition }}
     : { status: 404 };
 };
