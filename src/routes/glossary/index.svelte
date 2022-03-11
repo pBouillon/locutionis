bf40cfaee984ef7bd72cbd639e757353b6e2c105
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-03-11 22:29:54 +0100

    Create a glossary from the list of the definitions

--- src/routes/glossary/index.svelte
+++ src/routes/glossary/index.svelte
@@ -1,10 +1,27 @@
 <script lang="ts">
+import { normalize } from "$lib/functions/utils";
+
   import type { FigureOfSpeech } from "$lib/models/figure-of-speech";
 
 
   export let definitions: FigureOfSpeech[];
+
+  $: glossary = definitions.reduce(
+    (map, definition) => {
+      let key = definition.name[0].toLocaleLowerCase();
+      key = normalize(key);
+
+      map[key] = definition;
+      return map;
+    }, new Map<string, FigureOfSpeech[]>());
 </script>
 
-{#each definitions as definition}
-  <p>{definition.name}</p>
+<h1 class="text-primary text-xl mb-5">
+  Glossaire
+</h1>
+
+<pre>{JSON.stringify(glossary)}</pre>
+
+{#each Object.entries(glossary) as [ key, definition ]}
+  <p>[{key}] {definition.name}</p>
 {/each}
