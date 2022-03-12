a87530c45b606950fe6b84a8de72bb9f71eee3bc
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-03-12 10:34:14 +0100

    Add prettier rules

--- src/routes/glossary/index.svelte
+++ src/routes/glossary/index.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-  import GlossarySection from "$lib/components/glossary-section.svelte";
-  import { normalize } from "$lib/functions/utils";
-  import type { FigureOfSpeech } from "$lib/models/figure-of-speech";
+  import GlossarySection from '$lib/components/glossary-section.svelte';
+  import { normalize } from '$lib/functions/utils';
+  import type { FigureOfSpeech } from '$lib/models/figure-of-speech';
 
   export let definitions: FigureOfSpeech[];
 
   $: glossary = definitions
-    .map(definition => {
+    .map((definition) => {
       let key = definition.name[0].toLocaleLowerCase();
       key = normalize(key);
 
@@ -14,13 +14,12 @@
     })
     .reduce((map, { key, definition }) => {
       map[key] = map.hasOwnProperty(key)
-        ? [ ...map[key], definition ]
-        : [ definition ];
+        ? [...map[key], definition]
+        : [definition];
 
       return map;
     }, { });
 </script>
-
 
 <h1 class="mb-3 text-2xl font-extrabold tracking-tight md:text-4xl sm:text-center text-primary">
   Glossaire
