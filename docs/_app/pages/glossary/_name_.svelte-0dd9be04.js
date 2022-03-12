182b995a5a956d482d348e674218060e66d0a07d
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-03-12 10:43:39 +0100

    Regenerate the application's bundle

---
+++ docs/_app/pages/glossary/_name_.svelte-0dd9be04.js
@@ -0,0 +1,5 @@
+import{S as m,i as u,s as h,k as _,e as p,t as v,O as y,d as r,m as x,c as b,a as g,h as k,b as q,g as c,F as E,j as L,G as f}from"../../chunks/vendor-1b59dde5.js";function P(l){let a,n,t,s=l[0].name+"",o;return document.title=a=`\r
+    `+l[0].name+` | Locutionis - Petit r\xE9f\xE9rentiel des figures de style\r
+  `,{c(){n=_(),t=p("h1"),o=v(s),this.h()},l(e){y('[data-svelte="svelte-1754kyp"]',document.head).forEach(r),n=x(e),t=b(e,"H1",{class:!0});var d=g(t);o=k(d,s),d.forEach(r),this.h()},h(){q(t,"class","mb-3 text-2xl font-semibold text-primary")},m(e,i){c(e,n,i),c(e,t,i),E(t,o)},p(e,[i]){i&1&&a!==(a=`\r
+    `+e[0].name+` | Locutionis - Petit r\xE9f\xE9rentiel des figures de style\r
+  `)&&(document.title=a),i&1&&s!==(s=e[0].name+"")&&L(o,s)},i:f,o:f,d(e){e&&r(n),e&&r(t)}}}function S(l,a,n){let{definition:t}=a;return l.$$set=s=>{"definition"in s&&n(0,t=s.definition)},[t]}class B extends m{constructor(a){super();u(this,a,S,P,h,{definition:0})}}export{B as default};
