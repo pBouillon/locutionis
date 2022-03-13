9457454e8c0cd8292e65b2aea35adc50d99ea3e0
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-03-13 14:55:15 +0100

    Add the footer to the layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
   import '../app.css';
   import 'bootstrap-icons/font/bootstrap-icons.css';
+  import Footer from '$lib/components/footer.svelte';
   import Navbar from '$lib/components/navbar.svelte';
 </script>
 
-<Navbar />
+<div class="flex flex-col min-h-screen">
+  <Navbar />
 
-<main class="px-5 pt-2">
-  <slot />
-</main>
+  <main class="px-5 pt-2 flex-grow">
+    <slot />
+  </main>
+
+  <Footer />
+</div>
