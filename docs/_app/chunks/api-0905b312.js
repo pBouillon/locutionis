cb9a78c19dab7f6b8c274731d35a2437bdf86d79
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-03-13 16:38:05 +0100

    Regenerate the application's bundle

---
+++ docs/_app/chunks/api-0905b312.js
@@ -0,0 +1,29 @@
+const s=[{name:"Antanaclase",description:`
+    On parle d'antanaclase lorsque l'on emploie deux fois un m\xEAme mot mais avec
+    un sens diff\xE9rent: on parle alors de polys\xE9mie (plusieurs sens). Si le mot
+    n'est pas r\xE9p\xE9t\xE9 il est alors question d'antanaclase elliptique.
+    `,goal:`
+    Il s'agit d'une figure de style qui se pr\xEAte tr\xE8s bien aux jeux de mots,
+    elle est donc fr\xE9quemment utilis\xE9e pour faire de l'humour.
+
+    En dehors du c\xF4t\xE9 comique, elle permet de souligner le second sens du mot en
+    surprenant le lecteur qui doit s'arr\xEAter un instant pour le saisir apr\xE8s
+    l'avoir avoir compris le premier.
+    `,examples:["Le c\u0153ur a ses raisons que la raison ne conna\xEEt ignore.","Adieu, monsieur l'homme d'affaires, qui n'avez fait celles de personne.","Les \xE9tudiants, c'est comme le linge, quand il fait beau, \xE7a s\xE8che."],sources:[{name:"Wikipedia",href:"https://fr.wikipedia.org/wiki/Antanaclase"},{name:"Larousse",href:"https://www.larousse.fr/dictionnaires/francais/antanaclase/10911044"},{name:"La Culture G\xE9n\xE9rale",href:"https://www.laculturegenerale.com/antanaclase-diaphore-definition-exemples/"}]},{name:"Comparaison",description:`
+    Une comparaison est la mise en relation de deux \xE9l\xE9ments diff\xE9rents
+    partageant un point commun. Elle est constitu\xE9e d'un compar\xE9 (l'objet de
+    la comparaison), d'un comparant (le "th\xE8me" utilis\xE9 pour imager le compar\xE9)
+    et d'un outil de comparaison (c'est ce qui met en liaison le compar\xE9 et le
+    comparant).
+    `,goal:`
+    En mettant deux termes sur le m\xEAme plan litt\xE9raire, la comparaison permet de
+    souligner leurs points communs pour imager ses propos.
+    `,examples:["Et cette terre \xE9tait proche, et elle lui apparaissait comme un bouclier sur la mer sombre.","Tu es fait comme un rat!","Sa barbe \xE9tait d'argent comme un ruisseau d'avril."],sources:[{name:"Wikipedia",href:"https://fr.wikipedia.org/wiki/Comparaison_(rh\xE9torique)"},{name:"La langue fran\xE7aise",href:"https://www.lalanguefrancaise.com/linguistique/la-comparaison-figure-de-style"},{name:"alloprof",href:"https://www.alloprof.qc.ca/fr/eleves/bv/francais/la-comparaison-f1369"},{name:"Youtube - Point Culture",href:"https://youtu.be/ByDNEsBNf24?t=478"}]},{name:"\xC9panorthose",description:`
+    L'\xE9panorthose est une figure de style qui consiste \xE0 corriger ses propres
+    propos afin d'accentuer ce qu'il vient d'\xEAtre affirm\xE9, renfor\xE7ant ainsi le
+    sentiment exprim\xE9.
+    `,goal:`
+    Il s'agit d'une figure de style souvent utilis\xE9e pour donner un sentiment de
+    sinc\xE9rit\xE9 dans son discours. En se corrigeant, le locuteur donne l'impression
+    de rechercher la pr\xE9cision.
+    `,examples:["J'esp\xE8re, que dis-je ? Je suis s\xFBr qu'on vous rendra justice.","Votre prudence ou plut\xF4t votre l\xE2chet\xE9 nous ont perdu."],sources:[{name:"Wikipedia",href:"https://fr.wikipedia.org/wiki/\xC9panorthose"},{name:"Wiktionnaire",href:"https://fr.wiktionary.org/wiki/\xE9panorthose"}]}],n=(r=void 0)=>{let e=s;return r&&(e=e.filter(a=>a.name===r)),e.sort((a,i)=>a.name.localeCompare(i.name))};export{n as g};
