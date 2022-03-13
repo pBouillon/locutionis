3c0abb24ab17dd8ee2fd32a2c283054161083866
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-03-13 14:54:55 +0100

    Create the footer component

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,28 @@
+<footer class="flex flex-col items-center gap-2 py-3 border-t">
+  <p class="flex gap-1">
+    <a href="https://github.com/pbouillon/locutionis">
+      <i class="bi bi-github"></i>
+      Voir sur GitHub
+    </a>
+
+    -
+
+    <a href="https://github.com/pBouillon/locutionis/issues/new">
+      Signaler une erreur
+    </a>
+  </p>
+
+  <p>
+    Créé par <a href="pbouillon.github.io">Pierre Bouillon</a>
+  </p>
+</footer>
+
+<style>
+  a {
+    @apply text-primary;
+  }
+
+  p {
+    @apply text-xs text-gray-500;
+  }
+</style>
