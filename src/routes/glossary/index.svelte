8f3451af28ba1fc9fdb2cccf33d4df9363fc012e
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-03-16 20:04:28 +0100

    Rework the glossary to use a Map<TKey, TValue>

--- src/routes/glossary/index.svelte
+++ src/routes/glossary/index.svelte
@@ -9,19 +9,21 @@
 
 <script lang="ts">
   import GlossarySection from '$lib/components/glossary/glossary-section.svelte';
+  import SearchBar from '$lib/components/search-bar.svelte';
   import { normalize } from '$lib/functions/utils';
   import type { FigureOfSpeech } from '$lib/models/figure-of-speech';
 
-  export let definitions: FigureOfSpeech[];
+  export let definitions: FigureOfSpeech[] = [];
+  export let searchText: string = '';
 
   $: glossary = definitions.reduce((map, definition) => {
     let key = definition.name[0].toLocaleLowerCase();
     key = normalize(key);
 
-    map[key] = [...(map[key] ?? []), definition];
+    return map.set(key, [...(map[key] ?? []), definition]);
+  }, new Map<string, FigureOfSpeech[]>());
 
-    return map;
-  }, {});
+  // $: filtered = glossary.filter();
 </script>
 
 <svelte:head>
@@ -32,8 +34,12 @@
   Glossaire
 </h1>
 
+<div class="container my-5 md:my-8">
+  <SearchBar bind:searchText />
+</div>
+
 <div class="container flex flex-col gap-3 md:gap-6">
-  {#each Object.entries(glossary) as [key, definitions]}
+  {#each Array.from(glossary.entries()) as [key, definitions]}
     <GlossarySection {key} {definitions} />
   {/each}
 </div>
