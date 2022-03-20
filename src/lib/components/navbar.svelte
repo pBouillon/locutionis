364d4dc9e6f8685e91b8a6f0c4b9b33c6eec7e5a
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-03-20 10:42:14 +0100

    Cleanup the mobile navbar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,6 +1,5 @@
-<script lang='ts'>
+<script lang="ts">
   import { base } from '$app/paths';
-
 
   let showMobileMenu = false;
 
@@ -10,10 +9,13 @@
   ];
 </script>
 
-<nav>
+<nav class="flex items-baseline justify-between p-5">
   <div class="flex items-baseline gap-3">
-    <a href="{base}/" class="text-2xl font-bold tracking-wide md:text-4xl bg-gradient">
-      <i class="bi bi-journal-text"></i>
+    <a
+      href="{base}/"
+      class="text-2xl font-bold tracking-wide md:text-4xl text-transparent bg-clip-text bg-gradient-to-br from-indigo-600 to-primary"
+    >
+      <i class="bi bi-journal-text" />
       Locutionis
     </a>
 
@@ -22,8 +24,12 @@
     </p>
   </div>
 
-  <button type="button" class="main-button" on:click={() => (showMobileMenu = true)}>
-    <i class="px-1 text-3xl bi bi-list"></i>
+  <button
+    type="button"
+    class="md:hidden rounded flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-primary/25 focus:ring-offset-2 focus:ring-offset-slate-50"
+    on:click={() => (showMobileMenu = true)}
+  >
+    <i class="px-1 text-3xl bi bi-list" />
   </button>
 
   <div class="hidden gap-5 md:flex">
@@ -34,19 +40,25 @@
 
   {#if showMobileMenu}
     <div class="fixed inset-0 z-50 md:hidden">
-      <div class="overlay" on:click={() => (showMobileMenu = false)}>
-      </div>
+      <div
+        class="fixed inset-0 bg-black/20 backdrop-blur-sm"
+        on:click={() => (showMobileMenu = false)}
+      />
 
-      <div class="mobile-menu-container">
-        <div class="flex flex-col w-2/3 gap-5">
+      <div
+        class="fixed top-4 right-4 w-full max-w-xs bg-white rounded-lg shadow-lg p-6 text-base font-semibold text-slate-900"
+      >
+        <ul class="space-y-5">
           {#each links as { href, label }}
-            <a {href} class="navbar-link">{label}</a>
+            <li>
+              <a {href} class="navbar-link">{label}</a>
+            </li>
           {/each}
-        </div>
+        </ul>
 
-        <div class="flex items-start justify-end w-1/3">
+        <div class="absolute top-5 right-5">
           <button type="button" on:click={() => (showMobileMenu = false)}>
-            <i class="text-xl text-gray-400 bi bi-x-lg"></i>
+            <i class="text-xl text-gray-400 bi bi-x-lg" />
           </button>
         </div>
       </div>
@@ -55,27 +67,7 @@
 </nav>
 
 <style>
-  nav {
-    @apply flex items-baseline justify-between p-5;
-  }
-
-  .bg-gradient {
-    @apply text-transparent bg-clip-text bg-gradient-to-br from-indigo-600 to-primary;
-  }
-
-  .main-button {
-    @apply md:hidden rounded flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-primary/25 focus:ring-offset-2 focus:ring-offset-slate-50;
-  }
-
-  .mobile-menu-container {
-    @apply flex fixed top-4 right-4 w-full max-w-xs bg-white rounded-lg shadow-lg p-6 text-base font-semibold text-slate-900;
-  }
-
   .navbar-link {
     @apply text-lg tracking-wide decoration-2 decoration-primary hover:underline;
-  }
-
-  .overlay {
-    @apply fixed inset-0 bg-black/20 backdrop-blur-sm;
   }
 </style>
