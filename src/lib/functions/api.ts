e17db8ef8249a10a6df00c4db3c842e846236cfa
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-04-30 10:51:12 +0200

    Add the "alitération"

--- src/lib/functions/api.ts
+++ src/lib/functions/api.ts
@@ -451,6 +451,47 @@ const figuresOfSpeech: FigureOfSpeech[] = [
       },
     ],
   },
+  {
+    name: 'Alitération',
+    description: `
+    L'alitération est la répétition d'une ou de plusieurs consonnes ou plus
+    généralement d'un même son consonne.
+    `,
+    goal: `
+    Le sens de la répétition du son se trouve dans le contexte dans lequel il
+    est utilisé. Par exemple, pour décrire un éboulement, un son [r] répété
+    rappellera les roulements des rochers qui tombent.
+    `,
+    examples: [
+      {
+        text: `Pour qui sont ces serpents qui sifflent sur vos têtes ?`,
+        source: `Racine, Andromaque, acte V, scène 5`,
+      },
+      {
+        text: `
+        (...) l'onde de choc fractura le fémur d'enceinte et le
+        vent sabla cru le village à travers les jointures béantes du granit.
+        Sous mon casque, le son atroce du roc poncé perce, mes dents vibrent -
+        je plie contre Pietro, des aiguilles de quartz crissent sur son masque de contre.
+        `,
+        source: 'La horde du contrevent, Alain Damasio',
+      },
+      {
+        text: `Tamtam sculpté, tamtam tendu qui gronde sous les doigts du vainqueur.`,
+        source: 'Léopold Sédar Senghor, La femme noire',
+      },
+    ],
+    sources: [
+      {
+        name: 'Wikipédia',
+        href: 'https://fr.wikipedia.org/wiki/Alitération',
+      },
+      {
+        name: 'La Culture Générale',
+        href: 'https://www.laculturegenerale.com/alliteration-definition-exemples/',
+      },
+    ],
+  },
 ];
 
 export const get = (name: string | undefined = undefined): FigureOfSpeech[] => {
