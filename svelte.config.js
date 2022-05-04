194a9081cc09580f10f36c4d987cf69349d4331d
Charles van Amersfoort <79457016+chvanam@users.noreply.github.com>
2022-05-04 17:48:36 +0200

    Use vercel adapter to fix ssr

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 const dev = process.env.NODE_ENV === 'development';
@@ -15,7 +15,8 @@ const config = {
       assets: 'docs',
       fallback: 'index.html',
     }),
-  }
+  },
 };
 
 export default config;
+
