75148d29e7c86501bda80e91e0e57b7930537e6b
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-05-05 08:44:27 +0200

    Use the Vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
-
-const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,7 +13,7 @@ const config = {
       assets: 'docs',
       fallback: 'index.html',
     }),
-  }
+  },
 };
 
 export default config;
