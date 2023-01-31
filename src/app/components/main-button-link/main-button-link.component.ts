102de136192607e8b3176b93346f4c90a45bb693
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-01-31 21:55:31 +0100

    Add the main button link stories

--- src/app/components/main-button-link/main-button-link.component.ts
+++ src/app/components/main-button-link/main-button-link.component.ts
@@ -9,7 +9,7 @@ import { RouterLink } from '@angular/router'
     <div class="flex justify-center">
       <a
         [routerLink]="navigateTo"
-        class="flex h-12 w-full items-center justify-center rounded-lg bg-sky-500 px-6 font-semibold text-white hover:bg-sky-500/90 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 sm:w-1/4"
+        class="flex h-12 w-full items-center justify-center rounded-lg bg-sky-500 px-6 font-semibold text-white cursor-pointer hover:bg-sky-500/90 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 sm:w-1/4"
       >
         <ng-content />
       </a>
