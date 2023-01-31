e6906ca93cb43e558ae6f6e815df00192c798466
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-01-31 21:23:10 +0100

    Rework the introduction text

--- src/app/features/quiz/pages/quiz-home/quiz-home.component.ts
+++ src/app/features/quiz/pages/quiz-home/quiz-home.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core'
-import { SubtitleComponent } from 'src/app/components/subtitle/subtitle.component';
-import { TitleComponent } from 'src/app/components/title/title.component';
+import { SubtitleComponent } from 'src/app/components/subtitle/subtitle.component'
+import { TitleComponent } from 'src/app/components/title/title.component'
 
 @Component({
   selector: 'app-quiz-home',
   standalone: true,
   imports: [TitleComponent, SubtitleComponent],
   template: `
-    <app-title>Quiz</app-title>
-
-    <app-subtitle>
-      Les figures de style n'ont plus de secret pour vous ?
-      <br/>
-      <em>Challengez-vous</em> pour le vérifier !
-    </app-subtitle>
+      <app-subtitle>
+        <span class="sm:mt-10 sm:text-2xl">
+          Les figures de style n'ont plus de secret pour vous ?
+          <br />
+          <em>Challengez-vous</em> pour tester vos connaissances !
+        </span>
+      </app-subtitle>
   `
 })
 export class QuizHomeComponent {}
