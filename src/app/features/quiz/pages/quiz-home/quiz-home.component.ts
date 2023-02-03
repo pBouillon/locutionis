646c15015882f1a77670f0e109978f3445df03df
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-02-03 19:20:38 +0100

    Add the completion ratio

--- src/app/features/quiz/pages/quiz-home/quiz-home.component.ts
+++ src/app/features/quiz/pages/quiz-home/quiz-home.component.ts
@@ -2,8 +2,10 @@ import { AsyncPipe, NgIf } from '@angular/common'
 import { Component, inject } from '@angular/core'
 import { combineLatest, map } from 'rxjs'
 import { ErrorComponent } from 'src/app/components/error/error.component'
+import { ProgressBarComponent } from 'src/app/components/progress-bar/progress-bar.component'
 import { SubtitleComponent } from 'src/app/components/subtitle/subtitle.component'
 import { TitleComponent } from 'src/app/components/title/title.component'
+import { Quiz } from '../../models/quiz'
 import { QuizService } from '../../services/quiz/quiz.service'
 import { QuizGenerationComponent } from './components/quiz-generation/quiz-generation.component'
 import { QuizQuestionComponent } from './components/quiz-question/quiz-question.component'
@@ -15,10 +17,11 @@ import { QuizQuestionComponent } from './components/quiz-question/quiz-question.
     NgIf,
     AsyncPipe,
     ErrorComponent,
-    TitleComponent,
-    SubtitleComponent,
+    ProgressBarComponent,
     QuizQuestionComponent,
-    QuizGenerationComponent
+    QuizGenerationComponent,
+    SubtitleComponent,
+    TitleComponent
   ],
   template: `
     <app-title>Quiz</app-title>
@@ -46,7 +49,9 @@ import { QuizQuestionComponent } from './components/quiz-question/quiz-question.
 
           <!-- Ongoing -->
           <ng-template #ongoing>
-            <hr class="my-5 bg-gray-500 md:mx-auto md:w-1/2" />
+            <div class="my-5">
+              <app-progress-bar [completionRatio]="getCompletionRatio(vm.current!)" />
+            </div>
 
             <!-- Current Question -->
               <app-quiz-question
@@ -80,6 +85,10 @@ export class QuizHomeComponent {
       isLoading
     }))
   )
+
+  getCompletionRatio (quiz: Quiz): number {
+    return quiz.currentQuestionIndex / quiz.questions.length
+  }
 
   generateQuiz (questionsCount: number): void {
     this._quizService.generateQuiz(questionsCount)
