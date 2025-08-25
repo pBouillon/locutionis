36671e387a5315b00c752720b10a822cc3e87b7e
pBouillon <pro.pierre.bouillon@protonmail.com>
2025-08-25 18:39:43 +0200

    fix: move the favicon from static/ to assets/

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,13 @@
   import Footer from '$lib/components/Footer.svelte';
   import Navbar from '$lib/components/navbar/Navbar.svelte';
   import '../app.css';
-  import { resolve } from '$app/paths';
+  import logo from '$lib/assets/favicon.jpg';
 
   let { children } = $props();
 </script>
 
 <svelte:head>
-  <link rel="icon" href={resolve('/favicon.jpg')} />
+  <link rel="icon" href={logo} />
 </svelte:head>
 
 <Navbar />
