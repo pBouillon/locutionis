b6db2e1ab94a956acefa285962fcba5f5240e383
pBouillon <pro.pierre.bouillon@protonmail.com>
2025-08-25 18:18:11 +0200

    chore: setup CI on Pages

--- svelte.config.js
+++ svelte.config.js
@@ -4,12 +4,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
-	extensions: ['.svelte', '.svx'],
-	compilerOptions: { runes: true }
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: [vitePreprocess(), mdsvex()],
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/locutionis' : ''
+    }
+  },
+  extensions: ['.svelte', '.svx'],
+  compilerOptions: { runes: true }
 };
 
 export default config;
