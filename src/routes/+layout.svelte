22e9aabbc0cc8ccce67807b605ce6e91c3c7c313
pBouillon <pro.pierre.bouillon@protonmail.com>
2025-08-25 18:24:39 +0200

    fix: resolve the favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,13 @@
   import Footer from '$lib/components/Footer.svelte';
   import Navbar from '$lib/components/navbar/Navbar.svelte';
   import '../app.css';
+  import { resolve } from '$app/paths';
 
   let { children } = $props();
 </script>
 
 <svelte:head>
-  <link rel="icon" href="/favicon.jpg" />
+  <link rel="icon" href={resolve('/favicon.jpg')} />
 </svelte:head>
 
 <Navbar />
