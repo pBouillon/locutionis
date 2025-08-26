7a27b5a1d4af147feb488da9a63dfde79bf6e7b3
pBouillon <pro.pierre.bouillon@protonmail.com>
2025-08-26 08:48:21 +0200

    chore: setup basepath in CI and .nojekyll

--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,6 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://svelte.dev/docs/kit/integrations
-  // for more information about preprocessors
   preprocess: [vitePreprocess(), mdsvex()],
   kit: {
     adapter: adapter(),
