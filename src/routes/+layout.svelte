9d89efc09e34fb9001c55c345b1edbf99f463cc3
pBouillon <pro.pierre.bouillon@protonmail.com>
2025-10-05 10:55:16 +0200

    chore: cleanup imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
+  import logo from '$lib/assets/favicon.jpg';
   import Footer from '$lib/components/Footer.svelte';
   import Navbar from '$lib/components/navbar/Navbar.svelte';
+
   import '../app.css';
-  import logo from '$lib/assets/favicon.jpg';
 
   let { children } = $props();
 </script>
